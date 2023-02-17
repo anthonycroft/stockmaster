bb0d0a2624444bb297c576064149f20778a92d10
Anthony Croft <ajcsignups@gmail.com>
2023-02-17 10:20:57 +0000

    Improved validation of Stock Amount and Price input fields.

--- assets/js/script.js
+++ assets/js/script.js
@@ -545,10 +545,10 @@ $(document).ready(function() {
     } else if ($.trim($("#stock").val()) === "") {
       showAlert("Required Input","You must enter a value in the stock field!", 'warning', false)
       errorsFound = true;
-    } else if ($.trim($("#stock-amount").val()) === "") {
+    } else if ($.trim($("#stock-amount").val()) === "" || isNaN($("#stock-amount").val())) {
       showAlert("Required Input","You must enter a value in the stock amount field!", 'warning', false)
       errorsFound = true;
-    } else if ($.trim($("#price").val() === "")) {
+    } else if ($("#price").val() === "" || isNaN($("#price").val())) {
       showAlert("Required Input","You must enter a value in the price field!", 'warning', false)
       errorsFound = true;
     }
